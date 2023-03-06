3b1f122957f24c85648ca6d79a3a4e33df0beb5d
gchin97 <graceschinn@gmail.com>
2023-03-06 10:45:58 +0900

    10.py

---
+++ 10.0.py
@@ -0,0 +1,4 @@
+a = list(range(1, 10))
+print(a)
+b = list(range(2, 10, 1))
+print(b)
---
+++ 6.0.py
@@ -0,0 +1,13 @@
+# # 변수 만들기
+# x, y= 10, 20
+# x, y= y, x  #변수의 자리를 바꿔줌
+# x= None #빈 변수를 만들 때 할당 값
+# d= d+5 #항상 d라는 변수를 줘야함
+#
+# split 은 공백을 기준으로 분리하여 변수에 차례대로 저장함
+a, b = map(int, input("?").split())
+# a = int(a)
+# b = int(b)
+print(a)
+print(b)
+# map 은 정수로 변환해줌
