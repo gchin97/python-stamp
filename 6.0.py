85c0256141ed377d5cbd9e83fdc59920c86b2469
summerolie <graceschinn@gmail.com>
2023-06-23 11:14:39 +0900

    practice

--- 6.0.py
+++ 6.0.py
@@ -5,9 +5,24 @@
 # d= d+5 #항상 d라는 변수를 줘야함
 #
 # split 은 공백을 기준으로 분리하여 변수에 차례대로 저장함
-a, b = map(int, input("?").split())
+#a, b = map(int, input("?").split())
 # a = int(a)
 # b = int(b)
-print(a)
-print(b)
+#print(a)
+#print(b)
 # map 은 정수로 변환해줌
+limit = 155
+    # limit = 166
+
+    # 입력 : 신장(키) 목록
+height_list = [189, 154, 145, 178, 164, 150]
+    # height_list = [183, 155, 145, 187, 166, 180]
+
+count = 0
+    ####### 구현 시작 ################
+
+for i in range(0,height_list+1):
+    if height_list[i] > limit: 
+        count = count + 1
+    print(count)
+    ########구현 끝 #####
